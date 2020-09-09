25f3456f6d17b54b7e5c55fa37f8799aa6e02295
Niharika Jayanthi <niharika.jayanthi06@gmail.com>
2020-09-08 17:45:05 -0700

    Updates

---
+++ precache-manifest.345109ce9049e19126d3eb3c79d164c2.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "36296b3a8ebac5fc9f68073ea805d07d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d11b22313a1f00a063d2",
+    "url": "/static/css/main.eee1e620.chunk.css"
+  },
+  {
+    "revision": "f7328cf0fef38355c1b1",
+    "url": "/static/js/2.f07c3db5.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.f07c3db5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d11b22313a1f00a063d2",
+    "url": "/static/js/main.72385a2b.chunk.js"
+  },
+  {
+    "revision": "35ec47dfee4c65c275be",
+    "url": "/static/js/runtime-main.c6e37e88.js"
+  },
+  {
+    "revision": "ac55e35ae58a6fa261d489583defcddc",
+    "url": "/static/media/coder-girl.ac55e35a.png"
+  },
+  {
+    "revision": "b6cd10ee9349e8b1b9b914ba79dbd1cb",
+    "url": "/static/media/controll.b6cd10ee.png"
+  },
+  {
+    "revision": "39147405b3cbb6130f7daf9da4d18d71",
+    "url": "/static/media/mercury.39147405.png"
+  },
+  {
+    "revision": "98f7796adca1509161d179256a98cd78",
+    "url": "/static/media/resume.98f7796a.pdf"
+  }
+]);
